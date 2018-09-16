5726f9e4353a569e38a326619ef4f6a5e5740b5a
Artur <gordiartur@gmail.com>
2018-09-16 02:05:21 -0700

    Finished welcome page.

--- src/containers/landing/Landing.js
+++ src/containers/landing/Landing.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './Landing.css';
 import Welcome from '../../components/welcome/welcome';
-import Button from '../../components/button/button';
 
 class Landing extends Component {
   render() {
     return (
-      <div className="Landing">
-        <header className="App-header">
-          <h1 className="App-title">Landing</h1>
-        </header>
-        <Welcome/>
-        <Button/>
-      </div>
+      <header className="Landing-header">
+        <div className="Landing">
+          <Welcome/>
+          <Link className="btn btn-success btn-xl"to="/newPost" style={{ textDecoration: 'none'}}>Start</Link>
+        </div>
+      </header>
     );
   }
 }
