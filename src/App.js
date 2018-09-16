0ee8522c9a19a1210a44ba0db7b4175e7060f6f1
Castiel <castiel.andrew@gmail.com>
2018-09-15 17:59:23 -0700

    init commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom";
+import './App.css';
+import Landing from './containers/landing/Landing'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Route exact path="/" component = {Landing} />
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
